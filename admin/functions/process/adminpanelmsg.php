2eda7b81596f1771446d0e069ceea8df5e2fad26
drkthunder02 <mancuso.christopher@gmail.com>
2017-05-30 21:57:22 -0500

    members edit function modified

--- admin/functions/process/adminpanelmsg.php
+++ admin/functions/process/adminpanelmsg.php
@@ -17,10 +17,13 @@ function AdminPanelMsg($msg) {
         printf("<h2>Admin deleted successfully.</h2><br>");
     } else if ($msg == 'AdminAddFailDuplicate') {
         printf("<h2>Admin Account Add Failed.</h2><br>");
-        printf("<h2>User already has an admin account.<br>");
+        printf("<h2>User already has an admin account.</h2><br>");
     } else if ($msg == 'MemberDeleteSuccess') {
-        printf("<h2>Member Deleted successfully.<br>");
-    } 
+        printf("<h2>Member Deleted successfully.</h2><br>");
+    } else if ($msg == 'MemberDeleteTSFail') {
+        printf("<h2>Deleted member from the database.</h2><br>");
+        printf("<h2>Failed to kick member from teamspeak.</h2><br>");
+    }
     
     printf("</div></div>");
 }
--- admin/functions/process/memberedit.php
+++ admin/functions/process/memberedit.php
@@ -45,13 +45,43 @@ $userDel = $db->fetchRow('SELECT * FROM Users WHERE id= :id', array('id' => $del
 $userDelChar = $db->fetchRow('SELECT * FROM Characters WHERE CharacterID= :id', array('id' => $userDel['CharacterID']));
 //Delete the user from the database
 $db->delete('Users', array('id' => $delete));
+$msg = 'MemberDeleteSuccess';
+//Try to remove the user from teamspeak
+try {
+    $ts3_VirtualSever = Teamspeak3::factory($config->GetTSServerQuery());
+} catch (Teamspeak3_Exception $e) {
+    printf("Error: " . $e->getMessage() . " [A" . __LINE__ . "]");
+    $timestamp = gmdate('d.m.y H:i');
+    $entry = 'Failure to connect to teamspeak3 server';
+    AddLogEntry($db, $timestamp, $entry);
+}
+//Check if the client is online and kick if they are
+try {
+    $online = $ts3_VirtualServer->clientGetIdsByUid($userDel['TSUniqueID']);
+    try {
+        $ts3_VirtualServer->clientGetByUid($tsUniqueID)->Kick(TeamSpeak3::KICK_SERVER, "Teamspeak access revoked by ".$_SESSION["EVEOTSusername"].".");
+    } catch (Exception $ex) {
+        printf("FAILED. (Error: ".$e->getMessage().") [A".__LINE__."]");
+        $timestamp = gmdate('d.m.y H:i');
+        $entry = "Failed to get " . $userDelChar['Character'] . "'s TSUnique ID.";
+        AddLogEntry($db, $timestamp, $entry);
+        $msg = 'MemberDeleteTSFail';
+    }
+} catch (TeamSpeak3_Exception $e) {
+    printf("FAILED> (Error: " . $e->getMessage() . ") [A" . __LINE__ . "]");
+    $timestamp = gmdate('d.m.y H:i');
+    $entry = "Failed to get " . $userDelChar['Character'] . "'s uid for teamspeak.";
+    AddLogEntry($db, $timestamp, $entry);
+    $msg = 'MemberDeleteTSFail';
+}
+
 //Create the log entry
 $timestamp = gmdate('d.m.y H:i');
 $entry = $_SESSION['username'] . " deleted " . $userDelChar['Character'] . "'s teamspeak privileges.";
 AddLogEntry($db, $timestamp, $entry);
 //Redirect back to the admin_panel
 $location = 'http://' . $_SERVER['HTTP_HOST'];
-$location = $location . dirname($_SERVER['PHP_SELF']) . '/../../admin_panel.php?msg=MemberDeleteSuccess';
+$location = $location . dirname($_SERVER['PHP_SELF']) . '/../../admin_panel.php?msg=' . $msg;
 header("Location: $location");
 
 ?>
