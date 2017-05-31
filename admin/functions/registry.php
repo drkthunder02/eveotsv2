413bad94bd30705b17c1887c53031ac98e251a8a
drkthunder02 <mancuso.christopher@gmail.com>
2017-05-30 22:34:15 -0500

    fixed registry for admin panel

--- admin/functions/registry.php
+++ admin/functions/registry.php
@@ -11,8 +11,6 @@ require_once __DIR__.'/../../vendor/autoload.php';
 //Database Functions
 require_once __DIR__.'/../../functions/database/dbclose.php';
 require_once __DIR__.'/../../functions/database/dbopen.php';
-require_once __DIR__.'/../../functions/database/sqlcheck.php';
-require_once __DIR__.'/../../functions/database/sqlchecknames.php';
 
 //Class Functions
 require_once __DIR__.'/../../functions/class/sessions.php';
@@ -20,14 +18,14 @@ require_once __DIR__.'/../../functions/class/esi.php';
 
 //HTML Functions
 require_once __DIR__.'/../functions/html/printadminindexlogin.php';
+require_once __DIR__.'/../functions/html/printadminlogs.php';
 require_once __DIR__.'/../functions/html/printadmintable.php';
 require_once __DIR__.'/../functions/html/printhtmlheader.php';
-require_once __DIR__.'/../functions/html/printmainpanel.php';
 require_once __DIR__.'/../functions/html/printnavbar.php';
 require_once __DIR__.'/../functions/html/printwhitelist.php';
-require_once __DIR__.'/../functions/html/printwhitelistform.php';
 
 //Process Functions
 require_once __DIR__.'/../functions/process/addlogentry.php';
 require_once __DIR__.'/../functions/process/adminpanelmsg.php';
+require_once __DIR__.'/../functions/process/checklogin.php';
 require_once __DIR__.'/../functions/process/checksecuritylevel.php';
