5c8839ddcd220c0e9b776d9d25c11400c734b44a
drkthunder02 <mancuso.christopher@gmail.com>
2017-06-08 21:44:25 -0500

    blue count on member audit sql

--- admin/member_audit.php
+++ admin/member_audit.php
@@ -52,7 +52,7 @@ printf("</div>");
 
 $listAmount = 50;
 $userCount = $db->fetchColumn('SELECT COUNT(id) FROM Users');
-$blueCount = $db->fetchColumn('SELECT COUNT(id) FROM Users WHERE Blue=\"true\">');
+$blueCount = $db->fetchColumn('SELECT COUNT(id) FROM Users WHERE Blue=\"1\"');
 $userCountMinusBlues = $userCount - $blueCount;
 $exactPages = $userCount / $listAmount;
 $maxPages = ceil($exactPages);
