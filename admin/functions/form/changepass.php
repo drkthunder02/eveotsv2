8fe694ad512e6c991037b68d004333b52a25dc21
drkthunder02 <mancuso.christopher@gmail.com>
2017-07-22 23:32:32 -0500

    fixed redirect in changepass form file

--- admin/functions/form/changepass.php
+++ admin/functions/form/changepass.php
@@ -73,5 +73,5 @@ if($newPass == "" || $passConf == "") {
 DBClose($db);
 
 $location = 'http://' . $_SERVER['HTTP_HOST'];
-$location = $location . dirname($_SERVER['PHP_SELF']) . '/../admin_panel.php?msg=AdminPasswordSuccess';
+$location = $location . dirname($_SERVER['PHP_SELF']) . '/../../admin_panel.php?msg=AdminPasswordSuccess';
 header("Location: $location");
